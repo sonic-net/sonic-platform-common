b1b9169bf4cf5b3d0e4778dc51c03b99b15c0c6b
Kebo Liu <kebol@mellanox.com>
2018-07-30 10:35:19 +0800

    [sonic-xcvrd] add new daemon sonic-xcvrd to platform monitor (#17)
    * [sonic-xcvrd] add new daemon sonic-xcvrd to fetch Transceiver informations to DB
    * This new daemon will periodcally read DOM information from eeprom and post to DB
    * This new daemon will listen to the SFP plug in/out event and update the DB accordingly
    * fix typo
    * revise according to comments
    * add tx_power support
    * remove unnecessary try/except
    * add error handle for get wrong status from common API
    * fix review comments
    * rename the event, move event and timer inside the class
    * code readability fix

---
+++ sonic-xcvrd/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(
+    name='sonic-xcvrd',
+    version='1.0',
+    description='Transceiver monitoring daemon for SONiC',
+    license='Apache 2.0',
+    author='SONiC Team',
+    author_email='linuxnetdev@microsoft.com',
+    url='https://github.com/Azure/sonic-platform-daemons',
+    maintainer='Kebo Liu',
+    maintainer_email='kebol@mellanox.com',
+    scripts=[
+        'scripts/xcvrd',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: No Input/Output (Daemon)',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: System :: Hardware',
+    ],
+    keywords='sonic SONiC TRANSCEIVER transceiver daemon XCVRD xcvrd',
+)
