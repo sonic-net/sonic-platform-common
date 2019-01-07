e5d81558637790e964b3afd018d8210891b15681
Kevin(Shengkai) Wang <kevinw@mellanox.com>
2019-01-08 02:56:53 +0800

    [sonic-psud] add a new daemon sonic-psud to platform monitor (#20)
    * This new daemon will periodcally read PSU presence and status from sysfs and write to DB
    * CLI will get the PSU presence and status from DB directly.
    
    Signed-off-by: Kevin Wang <kevinw@mellanox.com>

---
+++ sonic-psud/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(
+    name='sonic-psud',
+    version='1.0',
+    description='PSU daemon for SONiC',
+    license='Apache 2.0',
+    author='SONiC Team',
+    author_email='linuxnetdev@microsoft.com',
+    url='https://github.com/Azure/sonic-platform-daemons',
+    maintainer='Kevin Wang',
+    maintainer_email='kevinw@mellanox.com',
+    scripts=[
+        'scripts/psud',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: No Input/Output (Daemon)',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: System :: Hardware',
+    ],
+    keywords='sonic SONiC psu PSU daemon psud PSUD',
+)
