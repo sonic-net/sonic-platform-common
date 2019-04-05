5e1056e07b011e83e276c9d5a5425b289cdb1fea
JohnFanDNI <44209930+JohnFanDNI@users.noreply.github.com>
2019-04-05 15:33:02 -0700

    Enhance new platform API (#19)
    * Changes of SONiC new platform API based on latest design
    
    * Moved get_change_event from DeviceBase to ChassisBase and ModuleBase
    
    * chassis_base.py : added get_change_event
    device_base.py : fixed typo
    fan_base.py : defined DEVICE_TYPE
    module_base.py : defined DEVICE_TYPE, fixed typo
    psu_base.py : defined DEVICE_TYPE
    sfp_base.py : defined DEVICE_TYPE, fixed typo, modified descriptions of set_power_override
    thermal_base.py : defined DEVICE_TYPE
    
    * module_base.py : modified get_change_event to return devices in nested dict
    
    * Added dom related status support for SFP, including temperature, voltage, TX bias, TX power and RX power
    
    * 1. Per channel return for TX bias, TX power and RX power in sfp_base.py.
    2. Support system EEPROM and serial number in chassis_base.py.
    
    * 1. Added SYS EEPROM functions to module_base.py.  2. Fixed typos.  3. Merge get_watchdog to return self._watchdog in chassis_base
    
    * 1. Fixed various typos.  2. Changed get_ampere to get_current, get_watt to get_power in psu_base.py
    
    * Changed get_system_eeprom return to use type code defined in ONIE TlvInfo EEPROM as the keys

--- sonic_platform_base/psu_base.py
+++ sonic_platform_base/psu_base.py
@@ -12,24 +12,96 @@ class PsuBase(device_base.DeviceBase):
     """
     Abstract base class for interfacing with a power supply unit
     """
+    # Device type definition. Note, this is a constant.
+    DEVICE_TYPE = "psu"
 
     # Possible fan status LED colors
     STATUS_LED_COLOR_GREEN = "green"
     STATUS_LED_COLOR_RED = "red"
     STATUS_LED_COLOR_OFF = "off"
 
-    # FanBase-derived object representing fan module available on the PSU
-    _fan = None
+    # List of FanBase-derived objects representing all fans
+    # available on the PSU
+    _fan_list = []
 
-    def get_fan(self):
+    def get_num_fans(self):
         """
-        Retrieves object representing the fan module contained in this PSU
+        Retrieves the number of fan modules available on this PSU
 
         Returns:
-            An object dervied from FanBase representing the fan module
-            contained in this PSU
+            An integer, the number of fan modules available on this PSU
         """
-        return self._fan
+        return len(self._fan_list)
+
+    def get_all_fans(self):
+        """
+        Retrieves all fan modules available on this PSU
+
+        Returns:
+            A list of objects derived from FanBase representing all fan
+            modules available on this PSU
+        """
+        return self._fan_list
+
+    def get_fan(self, index):
+        """
+        Retrieves fan module represented by (0-based) index <index>
+
+        Args:
+            index: An integer, the index (0-based) of the fan module to
+            retrieve
+
+        Returns:
+            An object dervied from FanBase representing the specified fan
+            module
+        """
+        fan = None
+
+        try:
+            fan = self._fan_list[index]
+        except IndexError:
+            sys.stderr.write("Fan index {} out of range (0-{})\n".format(
+                             index, len(self._fan_list)-1))
+
+        return fan
+
+    def get_voltage(self):
+        """
+        Retrieves current PSU voltage output
+
+        Returns:
+            A float number, the output voltage in volts, 
+            e.g. 12.1 
+        """
+        raise NotImplementedError
+
+    def get_current(self):
+        """
+        Retrieves present electric current supplied by PSU
+
+        Returns:
+            A float number, the electric current in amperes, e.g 15.4
+        """
+        raise NotImplementedError
+
+    def get_power(self):
+        """
+        Retrieves current energy supplied by PSU
+
+        Returns:
+            A float number, the power in watts, e.g. 302.6
+        """
+        raise NotImplementedError
+
+    def get_powergood_status(self):
+        """
+        Retrieves the powergood status of PSU
+
+        Returns:
+            A boolean, True if PSU has stablized its output voltages and passed all
+            its internal self-tests, False if not.
+        """
+        raise NotImplementedError
 
     def set_status_led(self, color):
         """
@@ -41,5 +113,17 @@ class PsuBase(device_base.DeviceBase):
 
         Returns:
             bool: True if status LED state is set successfully, False if not
+        """
+        raise NotImplementedError
+
+    def get_status_led(self, color):
+        """
+        Gets the state of the PSU status LED
+
+        Args:
+            color: A string representing the color of PSU status LED
+
+        Returns:
+            bool: True if get LED state returned successfully, False if not
         """
         raise NotImplementedError
---
+++ sonic_platform_base/sfp_base.py
@@ -0,0 +1,266 @@
+#
+# sfp_base.py
+#
+# Abstract base class for implementing a platform-specific class with which
+# to interact with a SFP module in SONiC
+#
+
+import sys
+from . import device_base
+
+
+class SfpBase(device_base.DeviceBase):
+    """
+    Abstract base class for interfacing with a SFP module
+    """
+    # Device type definition. Note, this is a constant.
+    DEVICE_TYPE = "sfp"
+
+    def get_transceiver_info(self):
+        """
+        Retrieves transceiver info of this SFP
+
+        Returns:
+            A dict which contains following keys/values :
+        ========================================================================
+        keys                       |Value Format   |Information	
+        ---------------------------|---------------|----------------------------
+        type                       |1*255VCHAR     |type of SFP
+        hardwarerev                |1*255VCHAR     |hardware version of SFP
+        serialnum                  |1*255VCHAR     |serial number of the SFP
+        manufacturename            |1*255VCHAR     |SFP vendor name
+        modelname                  |1*255VCHAR     |SFP model name
+        Connector                  |1*255VCHAR     |connector information
+        encoding                   |1*255VCHAR     |encoding information
+        ext_identifier             |1*255VCHAR     |extend identifier
+        ext_rateselect_compliance  |1*255VCHAR     |extended rateSelect compliance
+        cable_length               |INT            |cable length in m
+        mominal_bit_rate           |INT            |nominal bit rate by 100Mbs
+        specification_compliance   |1*255VCHAR     |specification compliance
+        vendor_date                |1*255VCHAR     |vendor date
+        vendor_oui                 |1*255VCHAR     |vendor OUI
+        ========================================================================
+        """
+        return NotImplementedError
+
+    def get_transceiver_bulk_status(self):
+        """
+        Retrieves transceiver bulk status of this SFP
+
+        Returns:
+            A dict which contains following keys/values :
+        ========================================================================
+        keys                       |Value Format   |Information	
+        ---------------------------|---------------|----------------------------
+        RX LOS                     |BOOLEAN        |RX lost-of-signal status,
+                                   |               |True if has RX los, False if not.
+        TX FAULT                   |BOOLEAN        |TX fault status,
+                                   |               |True if has TX fault, False if not.
+        Reset status               |BOOLEAN        |reset status,
+                                   |               |True if SFP in reset, False if not.
+        LP mode                    |BOOLEAN        |low power mode status,
+                                   |               |True in lp mode, False if not.
+        TX disable                 |BOOLEAN        |TX disable status,
+                                   |               |True TX disabled, False if not.
+        TX disabled channel        |HEX            |disabled TX channles in hex,
+                                   |               |bits 0 to 3 represent channel 0
+                                   |               |to channel 3.
+        Temperature                |INT            |module temperature in Celsius
+        Voltage                    |INT            |supply voltage in mV
+        TX bias                    |INT            |TX Bias Current in mA
+        RX power                   |INT            |received optical power in mW
+        TX power                   |INT            |TX output power in mW
+        ========================================================================
+        """
+        return NotImplementedError
+                
+    def get_reset_status(self):
+        """
+        Retrieves the reset status of SFP
+
+        Returns:
+            A Boolean, True if reset enabled, False if disabled
+        """
+        return NotImplementedError
+
+    def get_rx_los(self):
+        """
+        Retrieves the RX LOS (lost-of-signal) status of SFP
+
+        Returns:
+            A Boolean, True if SFP has RX LOS, False if not.
+            Note : RX LOS status is latched until a call to get_rx_los or a reset.
+        """
+        return NotImplementedError
+
+    def get_tx_fault(self):
+        """
+        Retrieves the TX fault status of SFP
+
+        Returns:
+            A Boolean, True if SFP has TX fault, False if not
+            Note : TX fault status is lached until a call to get_tx_fault or a reset.
+        """
+        return NotImplementedError
+
+    def get_tx_disable(self):
+        """
+        Retrieves the tx_disable status of this SFP
+
+        Returns:
+            A Boolean, True if tx_disable is enabled, False if disabled
+        """
+        return NotImplementedError
+
+    def get_tx_disable_channel(self):
+        """
+        Retrieves the TX disabled channels in this SFP
+
+        Returns:
+            A hex of 4 bits (bit 0 to bit 3 as channel 0 to channel 3) to represent
+            TX channels which have been disabled in this SFP.
+            As an example, a returned value of 0x5 indicates that channel 0 
+            and channel 2 have been disabled.
+        """
+        return NotImplementedError
+
+    def get_lpmode(self):
+        """
+        Retrieves the lpmode (low power mode) status of this SFP
+
+        Returns:
+            A Boolean, True if lpmode is enabled, False if disabled
+        """
+        return NotImplementedError
+
+    def get_power_override(self):
+        """
+        Retrieves the power-override status of this SFP
+
+        Returns:
+            A Boolean, True if power-override is enabled, False if disabled
+        """
+        return NotImplementedError
+    
+    def get_temperature(self):
+        """
+        Retrieves the temperature of this SFP
+
+        Returns:
+            An integer number of current temperature in Celsius
+        """
+        return NotImplementedError
+
+
+    def get_voltage(self):
+        """
+        Retrieves the supply voltage of this SFP
+
+        Returns:
+            An integer number of supply voltage in mV
+        """
+        return NotImplementedError
+    
+    def get_tx_bias(self):
+        """
+        Retrieves the TX bias current of this SFP
+
+        Returns:
+            A list of four integer numbers, representing TX bias in mA
+            for channel 0 to channel 4.
+            Ex. ['110.09', '111.12', '108.21', '112.09']
+        """
+        return NotImplementedError
+    
+    def get_rx_power(self):
+        """
+        Retrieves the received optical power for this SFP
+
+        Returns:
+            A list of four integer numbers, representing received optical
+            power in mW for channel 0 to channel 4.
+            Ex. ['1.77', '1.71', '1.68', '1.70']
+        """
+        return NotImplementedError
+    
+    def get_tx_power(self):
+        """
+        Retrieves the TX power of this SFP
+
+        Returns:
+            A list of four integer numbers, representing TX power in mW
+            for channel 0 to channel 4.
+            Ex. ['1.86', '1.86', '1.86', '1.86']
+        """
+        return NotImplementedError
+    
+    def reset(self):
+        """
+        Reset SFP and return all user module settings to their default srate.
+
+        Returns:
+            A boolean, True if successful, False if not
+        """
+        return NotImplementedError
+    
+    def tx_disable(self, tx_disable):
+        """
+        Disable SFP TX for all channels
+
+        Args:
+            tx_disable : A Boolean, True to enable tx_disable mode, False to disable
+                         tx_disable mode.
+
+        Returns:
+            A boolean, True if tx_disable is set successfully, False if not
+        """
+        return NotImplementedError
+    
+    def tx_disable_channel(self, channel, disable):
+        """
+        Sets the tx_disable for specified SFP channels
+
+        Args:
+            channel : A hex of 4 bits (bit 0 to bit 3) which represent channel 0 to 3,
+                      e.g. 0x5 for channel 0 and channel 2.
+            disable : A boolean, True to disable TX channels specified in channel,
+                      False to enable
+
+        Returns:
+            A boolean, True if successful, False if not
+        """
+        return NotImplementedError
+    
+    def set_lpmode(self, lpmode):
+        """
+        Sets the lpmode (low power mode) of SFP
+
+        Args:
+            lpmode: A Boolean, True to enable lpmode, False to disable it
+            Note  : lpmode can be overridden by set_power_override
+
+        Returns:
+            A boolean, True if lpmode is set successfully, False if not
+        """
+        return NotImplementedError
+    
+    def set_power_override(self, power_override, power_set):
+        """
+        Sets SFP power level using power_override and power_set
+
+        Args:
+            power_override : 
+                    A Boolean, True to override set_lpmode and use power_set
+                    to control SFP power, False to disable SFP power control
+                    through power_override/power_set and use set_lpmode
+                    to control SFP power.
+            power_set :
+                    Only valid when power_override is True.
+                    A Boolean, True to set SFP to low power mode, False to set
+                    SFP to high power mode.
+
+        Returns:
+            A boolean, True if power-override and power_set are set successfully,
+            False if not
+        """
+        return NotImplementedError
