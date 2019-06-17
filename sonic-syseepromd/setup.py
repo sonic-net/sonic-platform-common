84bca6465b5d089bb40fb47d7c4006b7558e1091
Kebo Liu <kebol@mellanox.com>
2019-06-18 02:09:52 +0800

    [syseeprom] Add a new daemon to populate syseeprom info to state DB (#25)

---
+++ sonic-syseepromd/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(
+    name='sonic-syseepromd',
+    version='1.0',
+    description='Syseeprom gathering daemon for SONiC',
+    license='Apache 2.0',
+    author='SONiC Team',
+    author_email='linuxnetdev@microsoft.com',
+    url='https://github.com/Azure/sonic-platform-daemons',
+    maintainer='Kebo Liu',
+    maintainer_email='kebol@mellanox.com',
+    scripts=[
+        'scripts/syseepromd',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: No Input/Output (Daemon)',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: System :: Hardware',
+    ],
+    keywords='sonic SONiC SYSEEPROM syseeprom SYSEEPROMD syseepromd',
+)
