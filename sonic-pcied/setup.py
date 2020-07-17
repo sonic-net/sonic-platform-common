e665ee8767902ab5f4d5b8d7606da64bf30f099a
Sujin Kang <sujkang@microsoft.com>
2020-07-17 16:34:09 -0700

    Initial version of pcied (#60)
    * Initial version of pcied
    
    * Fix LGTM error
    
    * Fix LGTM issues
    
    * Update to read the state_db for the pcie device status check after
    calling `pcieutil pcie-check`.
    
    * fix LGTM
    
    * Update the pcie_status in STATE_DB
    
    * Add logic to exit the daemon if the pcie configuration file doesn't exist.
    
    * review comments - spaces
    
    * review comments
    
    * fixed lgtm
    
    * typo
    
    * typo
    
    * use subprocess call
    
    * review comment

---
+++ sonic-pcied/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(
+    name='sonic-pcied',
+    version='1.0',
+    description='PCIe check daemon for SONiC',
+    license='Apache 2.0',
+    author='SONiC Team',
+    author_email='linuxnetdev@microsoft.com',
+    url='https://github.com/Azure/sonic-platform-daemons',
+    maintainer='Sujin Kang',
+    maintainer_email='sujkang@microsoft.com',
+    scripts=[
+        'scripts/pcied',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: No Input/Output (Daemon)',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: System :: Hardware',
+    ],
+    keywords='sonic SONiC PCIe pcie PCIED pcied',
+)
