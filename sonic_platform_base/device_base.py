5743ba5d382d07088bdd8358430669dc1107c253
junchao <junchao@nvidia.com>
2020-09-11 13:37:57 +0800

    Adjust comment to make it more accurate

--- sonic_platform_base/device_base.py
+++ sonic_platform_base/device_base.py
@@ -59,9 +59,10 @@ class DeviceBase(object):
 
     def get_position_in_parent(self):
 		"""
-		Retrieves 1-based relative physical position in parent device
+		Retrieves 1-based relative physical position in parent device. If the agent cannot determine the parent-relative position
+        for some reason, or if the associated value of entPhysicalContainedIn is '0', then the value '-1' is returned
 		Returns:
-		    integer: The 1-based relative physical position in parent device
+		    integer: The 1-based relative physical position in parent device or -1 if cannot determine the position
 		"""
 		raise NotImplementedError
 
