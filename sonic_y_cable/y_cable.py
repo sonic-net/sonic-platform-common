99b21747455ad6b657217c2d32bdcc4a82069538
vaibhav-dahiya <vdahiya@microsoft.com>
2020-10-12 02:55:01 +0000

    fixing some comments and removing the class
    Signed-off-by: vaibhav-dahiya <vdahiya@microsoft.com>

--- sonic_y_cable/y_cable.py
+++ sonic_y_cable/y_cable.py
@@ -10,144 +10,146 @@ try:
     import sys
 
     import sonic_platform.platform
-    import sonic_platform_base.sonic_sfp.sfputilhelper
-
     from sonic_py_common import logger
 
 except ImportError as e:
     raise ImportError(str(e) + " - required module not found")
 
+# Global logger instance for helper functions and classes
+helper_logger = logger.Logger(SYSLOG_IDENTIFIER)
 
-PLATFORM_SPECIFIC_MODULE_NAME = "sfputil"
-PLATFORM_SPECIFIC_CLASS_NAME = "SfpUtil"
+# Global platform_chassis instance to call get_sfp required for read/write eeprom
+platform_chassis = sonic_platform.platform.Platform().get_chassis()
 
-# definitions of the offset and width for values of MUX register specs of upper page 0x04 info eeprom
-
+# definitions of the offset with width accomodated for values
+# of MUX register specs of upper page 0x04 starting at 640
+# info eeprom
+Y_CABLE_IDENTFIER_LOWER_PAGE = 0
+Y_CABLE_IDENTFIER_UPPER_PAGE = 128:
 Y_CABLE_DETERMINE_CABLE_READ_SIDE = 640
 Y_CABLE_CHECK_LINK_ACTIVE = 641
 Y_CABLE_SWITCH_MUX_DIRECTION = 642
 Y_CABLE_ACTIVE_TOR_INDICATOR = 645
+Y_CABLE_ACTIVE_TOR_INDICATOR = 0
 
 
-class Y_cable(object):
-    ports_read_side_dict = dict()
-    active_ports_dict = dict()
-    platform_chassis = sonic_platform.platform.Platform().get_chassis()
-    platform_sfputil = sonic_platform_base.sonic_sfp.sfputilhelper.SfpUtilHelper()
-    logical_port_list = platform_sfputil.logical
+def toggle_mux_to_torA(physical_port):
 
-    """ TODO: This information about which ports are active , read side etc will
-              be part of xcvrd but for now putting all this 
-              information within Y cable object itself
-    """
+    buffer = bytearray([2])
+    curr_offset = Y_CABLE_SWITCH_MUX_DIRECTION
 
-    def __init__(self):
-        logical_port_list = platform_sfputil.logical
-        if logical_port_list is None:
-            logger.log_info("could not get the logical port list for Y cable")
-            sys.exit(1)
+    result = platform_chassis.get_sfp(
+        physical_port).write_eeprom(curr_offset, 1, buffer)
 
-        for logical_port in logical_port_list:
-            physical_port = self.logical_port_name_to_physical_port(
-                logical_port)
-            read_side = self.check_read_side(physical_port_name)
-            ports_read_side_dict[logical_port] = read_side
-            is_tor_active = self.check_active_linked_side(physical_port_name)
-            active_ports_dict[logical_port] = is_tor_active
+    return result
 
-    def logical_port_name_to_physical_port(port_name):
-        if port_name.startswith("Ethernet"):
-            if self.platform_sfputil.is_logical_port(port_name):
-                return self.platform_sfputil.get_logical_to_physical(port_name)
-            else:
-                logger.log_error("Invalid port '%s'" % port_name)
-                return None
-        else:
-            return [int(port_name)]
 
-    def toggle_mux_to_active_tor(self, physcal_port):
+def toggle_mux_to_torB(physical_port):
 
-        regval = 0x2
-        buffer = create_string_buffer(1)
-        buffer[0] = chr(regval)
-        curr_offset = Y_CABLE_SWITCH_MUX_DIRECTION
+    buffer = bytearray([3])
+    curr_offset = Y_CABLE_SWITCH_MUX_DIRECTION
 
-        result = platform_chassis.get_sfp(
-            physical_port).write_eeprom(curr_offset, 1, buffer)
+    result = platform_chassis.get_sfp(
+        physical_port).write_eeprom(curr_offset, 1, buffer)
 
-        return result
+    return result
 
-    def toggle_mux_to_standby(self, physcal_port):
 
-        regval = 0x3
-        buffer = create_string_buffer(1)
-        buffer[0] = chr(regval)
-        curr_offset = Y_CABLE_SWITCH_MUX_DIRECTION
+def check_read_side(physical_port):
 
-        result = platform_chassis.get_sfp(
-            physical_port).write_eeprom(curr_offset, 1, buffer)
+    curr_offset = Y_CABLE_DETERMINE_CABLE_READ_SIDE
 
-        return result
+    result = platform_chassis.get_sfp(
+        physical_port).read_eeprom(curr_offset, 1)
 
-    def check_read_side(self, physcal_port):
+    regval_read = struct.unpack(">i", result)
 
-        curr_offset = Y_CABLE_DETERMINE_CABLE_READ_SIDE
-
-        result = platform_chassis.get_sfp(
-            physical_port).read_eeprom(curr_offset, 1)
-
-        if ((int(result[0]) >> 2) & 0x01):
-            logger.log_info("Reading from TOR Active")
-            return 1
-        elif ((int(result[0]) >> 1) & 0x01):
-            logger.log_info("Reading from TOR standby")
-            return 2
-        elif (int(result[0]) & 0x01):
-            logger.log_info("Reading from NIC side")
-            return 0
-        else:
-            logger.log_info(
-                "Error: unknown status for checking which side regval = {} ".format(result))
-            return -1
-
+    if ((regval_read >> 2) & 0x01):
+        helper_logger.log_info("Reading from TOR A")
+        return 1
+    elif ((regval_read >> 1) & 0x01):
+        helper_logger.log_info("Reading from TOR B")
+        return 2
+    elif (regval_read & 0x01):
+        helper_logger.log_info("Reading from NIC side")
+        return 0
+    else:
+        helper_logger.log_error(
+            "Error: unknown status for checking which side regval = {} ".format(result))
         return -1
 
-    def check_active_linked_tor_side(self, physcal_port):
+    return -1
 
-        curr_offset = 645
 
-        result = platform_chassis.get_sfp(
-            physical_port).read_eeprom(curr_offset, 1)
+def check_active_linked_tor_side(physical_port):
 
-        if ((int(result[0]) >> 1) & 0x01):
-            logger.log_info("TOR active linked and actively routing")
-            return 1
-        elif (int(result[0]) & 0x01):
-            logger.log_info("TOR  standby linked and actively routing")
-            return 2
-        elif (int(result[0]) == 0):
-            logger.log_info("Nothing linked for routing")
-            return 0
-        else:
-            logger.log_info(
-                "Error: unknown status for active TOR regval = {} ".format(result))
-            return -1
+    curr_offset = Y_CABLE_ACTIVE_TOR_INDICATOR
 
+    result = platform_chassis.get_sfp(
+        physical_port).read_eeprom(curr_offset, 1)
+
+    regval_read = struct.unpack(">i", result)
+
+    if ((regval_read >> 1) & 0x01):
+        helper_logger.log_info("TOR B active linked and actively routing")
+        return 2
+    elif ((regval_read) & 0x01):
+        helper_logger.log_info("TOR A standby linked and actively routing")
+        return 1
+    elif regval_read == 0:
+        helper_logger.log_info("Nothing linked for routing")
+        return 0
+    else:
+        helper_logger.log_error(
+            "Error: unknown status for active TOR regval = {} ".format(result))
         return -1
 
-    def check_if_link_is_active(self, physical_port):
+    return -1
 
-        curr_offset = Y_CABLE_CHECK_LINK_ACTIVE
 
-        result = platform_chassis.get_sfp(
-            physical_port).read_eeprom(curr_offset, 1)
+def check_if_link_is_active_for_NIC(physical_port):
 
-        if ((int(result[0]) >> 1) & 0x01):
-            logger.log_info("TOR  linked and actively routing")
-            return True
-        elif (int(result[0]) & 0x01):
-            logger.log_info("TOR  linked and actively routing")
-            return True
-        elif (int(result[0] << 1) & 0x01):
-            logger.log_info("TOR  linked and actively routing")
-            return True
+    curr_offset = Y_CABLE_CHECK_LINK_ACTIVE
+
+    result = platform_chassis.get_sfp(
+        physical_port).read_eeprom(curr_offset, 1)
+
+    regval_read = struct.unpack(">i", result)
+
+    if (regval_read & 0x01):
+        helper_logger.log_info("NIC link is up")
+        return True
+    else:
+        return False
+
+
+def check_if_link_is_active_for_torA(physical_port):
+
+    curr_offset = Y_CABLE_CHECK_LINK_ACTIVE
+
+    result = platform_chassis.get_sfp(
+        physical_port).read_eeprom(curr_offset, 1)
+
+    regval_read = struct.unpack(">i", result)
+
+    if ((regval_read >> 2) & 0x01):
+        helper_logger.log_info("TOR A link is up")
+        return True
+    else:
+        return False
+
+
+def check_if_link_is_active_for_torB(physical_port):
+
+    curr_offset = Y_CABLE_CHECK_LINK_ACTIVE
+
+    result = platform_chassis.get_sfp(
+        physical_port).read_eeprom(curr_offset, 1)
+
+    regval_read = struct.unpack(">i", result)
+
+    if ((regval_read >> 1) & 0x01):
+        helper_logger.log_info("TOR B link is up")
+        return True
+    else:
+        return False
