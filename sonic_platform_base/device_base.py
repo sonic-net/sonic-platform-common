16e4fe74f519c432e74aad677ee7dac7850951f3
junchao <junchao@nvidia.com>
2020-10-21 09:28:48 +0800

    Change tab to spaces

--- sonic_platform_base/device_base.py
+++ sonic_platform_base/device_base.py
@@ -58,18 +58,18 @@ class DeviceBase(object):
         raise NotImplementedError
 
     def get_position_in_parent(self):
-		"""
-		Retrieves 1-based relative physical position in parent device. If the agent cannot determine the parent-relative position
+        """
+        Retrieves 1-based relative physical position in parent device. If the agent cannot determine the parent-relative position
         for some reason, or if the associated value of entPhysicalContainedIn is '0', then the value '-1' is returned
-		Returns:
-		    integer: The 1-based relative physical position in parent device or -1 if cannot determine the position
-		"""
-		raise NotImplementedError
+        Returns:
+            integer: The 1-based relative physical position in parent device or -1 if cannot determine the position
+        """
+        raise NotImplementedError
 
     def is_replaceable(self):
-		"""
-		Indicate whether this device is replaceable.
-		Returns:
-		    bool: True if it is replaceable.
-		"""
-		raise NotImplementedError
+        """
+        Indicate whether this device is replaceable.
+        Returns:
+            bool: True if it is replaceable.
+        """
+        raise NotImplementedError
