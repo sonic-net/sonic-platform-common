a6592194557bf1ec97dd77f8f90090642f505344
vdahiya12 <67608553+vdahiya12@users.noreply.github.com>
2020-10-26 10:57:28 -0700

    [SONIC_SFP] adding abstract methods for reading and writing the eeprom address space within platform api (#126)
    * [sonic_sfp] adding abstract methods for platform api's
    
    Signed-off-by: vaibhav-dahiya <vdahiya@microsoft.com>

--- sonic_platform_base/sfp_base.py
+++ sonic_platform_base/sfp_base.py
@@ -356,3 +356,39 @@ class SfpBase(device_base.DeviceBase):
             False if not
         """
         raise NotImplementedError
+
+    def read_eeprom(self, offset, num_bytes):
+        """
+        read eeprom specfic bytes beginning from a random offset with size as num_bytes
+
+        Args:
+             offset :
+                     Integer, the offset from which the read transaction will start
+             num_bytes:
+                     Integer, the number of bytes to be read
+
+        Returns:
+            bytearray, if raw sequence of bytes are read correctly from the offset of size num_bytes
+            None, if the read_eeprom fails
+        """
+        raise NotImplementedError
+
+    def write_eeprom(self, offset, num_bytes, write_buffer):
+        """
+        write eeprom specfic bytes beginning from a random offset with size as num_bytes 
+        and write_buffer as the required bytes
+
+        Args:
+             offset :
+                     Integer, the offset from which the read transaction will start
+             num_bytes:
+                     Integer, the number of bytes to be written
+             write_buffer:
+                     bytearray, raw bytes buffer which is to be written beginning at the offset
+
+        Returns:
+            a Boolean, true if the write succeeded and false if it did not succeed.
+        """
+        raise NotImplementedError
+
+
