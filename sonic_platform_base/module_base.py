e61098677972ec8f9205467a95f615235a6601d9
mprabhu <manjunath.prabhu@nokia.com>
2020-10-28 13:54:54 -0700

    Fixing comments section in module_base.py

--- sonic_platform_base/module_base.py
+++ sonic_platform_base/module_base.py
@@ -23,27 +23,27 @@ class ModuleBase(device_base.DeviceBase):
     MODULE_TYPE_FABRIC  = "FABRIC-CARD"
 
     # Possible card status for modular chassis
-    #Module state is Empty if no module is inserted in the slot
+    # Module state is Empty if no module is inserted in the slot
     MODULE_STATUS_EMPTY   = "Empty"
-    #Module state if Offline if powered down. This is also the admin-down state.
+    # Module state if Offline if powered down. This is also the admin-down state.
     MODULE_STATUS_OFFLINE = "Offline"
-    #Module state is Present when it is powered up, but not fully functional.
+    # Module state is Present when it is powered up, but not fully functional.
     MODULE_STATUS_PRESENT = "Present"
-    #Module state is Present when it is powered up, but entered a fault state.
-    #Module is not able to go Online.
+    # Module state is Present when it is powered up, but entered a fault state.
+    # Module is not able to go Online.
     MODULE_STATUS_FAULT   = "Fault"
-    #Module state is Online when fully operational
+    # Module state is Online when fully operational
     MODULE_STATUS_ONLINE  = "Online"
 
-    #Invalid slot for modular chassis
+    # Invalid slot for modular chassis
     MODULE_INVALID_SLOT = -1
 
-    #Possible reboot types for modular chassis
-    #Module reboot type to reboot entire card
+    # Possible reboot types for modular chassis
+    # Module reboot type to reboot entire card
     MODULE_REBOOT_DEFAULT = "Default"
-    #Module reboot type to reboot CPU complex
+    # Module reboot type to reboot CPU complex
     MODULE_REBOOT_CPU_COMPLEX = "CPU"
-    #Module reboot type to reboot FPGA complex
+    # Module reboot type to reboot FPGA complex
     MODULE_REBOOT_FPGA_COMPLEX = "FPGA"
 
     # List of ComponentBase-derived objects representing all components
