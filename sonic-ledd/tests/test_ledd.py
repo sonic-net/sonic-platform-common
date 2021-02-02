8bf0fd1f65247d68b3a051fb7b655a828c7371d6
Joe LeVeque <jleveque@users.noreply.github.com>
2021-02-02 13:39:54 -0800

    [ledd] Refactor to allow for more thorough unit testing; Increase unit test coverage (#147)
    - Refactor ledd to allow for more thorough unit testing
        - Remove infinite loop from `run()` method to allow unit testing of the method. Instead, call the `run()` method in an infinite loop in `main()`
    - Increase overall ledd unit test coverage from 48% to 93%.
    
    Previous unit test coverage:
    
    ```
    ----------- coverage: platform linux, python 3.7.3-final-0 -----------
    Name           Stmts   Miss  Cover
    ----------------------------------
    scripts/ledd      66     34    48%
    Coverage HTML written to dir htmlcov
    Coverage XML written to file coverage.xml
    ```
    
    Unit test coverage with this patch:
    
    ```
    ----------- coverage: platform linux, python 3.7.3-final-0 -----------
    Name           Stmts   Miss  Cover
    ----------------------------------
    scripts/ledd      72      5    93%
    Coverage HTML written to dir htmlcov
    Coverage XML written to file coverage.xml
    ```

--- sonic-ledd/tests/test_ledd.py
+++ sonic-ledd/tests/test_ledd.py
@@ -5,29 +5,25 @@ from imp import load_source
 import pytest
 # TODO: Clean this up once we no longer need to support Python 2
 if sys.version_info.major == 3:
-    from unittest.mock import Mock, MagicMock, patch
+    from unittest import mock
 else:
-    from mock import Mock, MagicMock, patch
+    import mock
 from sonic_py_common import daemon_base
 
-SYSLOG_IDENTIFIER = 'ledd_test'
-NOT_AVAILABLE = 'N/A'
-
-daemon_base.db_connect = MagicMock()
+daemon_base.db_connect = mock.MagicMock()
 
 test_path = os.path.dirname(os.path.abspath(__file__))
 modules_path = os.path.dirname(test_path)
 scripts_path = os.path.join(modules_path, "scripts")
 sys.path.insert(0, modules_path)
 
-os.environ["LEDD_UNIT_TESTING"] = "1"
 load_source('ledd', scripts_path + '/ledd')
 import ledd
 
 
 def test_help_args(capsys):
     for flag in ['-h', '--help']:
-        with patch.object(sys, 'argv', ['ledd', flag]):
+        with mock.patch.object(sys, 'argv', ['ledd', flag]):
             with pytest.raises(SystemExit) as pytest_wrapped_e:
                 ledd.main()
             assert pytest_wrapped_e.type == SystemExit
@@ -38,7 +34,7 @@ def test_help_args(capsys):
 
 def test_version_args(capsys):
     for flag in ['-v', '--version']:
-        with patch.object(sys, 'argv', ['ledd', flag]):
+        with mock.patch.object(sys, 'argv', ['ledd', flag]):
             with pytest.raises(SystemExit) as pytest_wrapped_e:
                 ledd.main()
             assert pytest_wrapped_e.type == SystemExit
@@ -49,7 +45,7 @@ def test_version_args(capsys):
 
 def test_bad_args(capsys):
     for flag in ['-n', '--nonexistent']:
-        with patch.object(sys, 'argv', ['ledd', flag]):
+        with mock.patch.object(sys, 'argv', ['ledd', flag]):
             with pytest.raises(SystemExit) as pytest_wrapped_e:
                 ledd.main()
             assert pytest_wrapped_e.type == SystemExit
@@ -63,6 +59,93 @@ class TestDaemonLedd(object):
     Test cases to cover functionality in DaemonLedd class
     """
 
-    def test_run(self):
-        daemon_ledd = ledd.DaemonLedd(SYSLOG_IDENTIFIER)
-        # TODO: Add more coverage
+    def test_run_fail_load_platform_util(self):
+        with pytest.raises(SystemExit) as pytest_wrapped_e:
+            ledd.DaemonLedd()
+        assert pytest_wrapped_e.type == SystemExit
+        assert pytest_wrapped_e.value.code == ledd.LEDUTIL_LOAD_ERROR
+
+    @mock.patch("ledd.DaemonLedd.load_platform_util")
+    @mock.patch("ledd.swsscommon.SubscriberStateTable")
+    @mock.patch("ledd.swsscommon.Select")
+    def test_run_select_timeout(self, mock_select, mock_sst, mock_load_plat_util):
+        select_instance = mock_select.return_value
+        select_instance.select.return_value = (ledd.swsscommon.Select.TIMEOUT, None)
+
+        daemon_ledd = ledd.DaemonLedd()
+        ret = daemon_ledd.run()
+        assert ret == 1
+
+    @mock.patch("ledd.DaemonLedd.load_platform_util")
+    @mock.patch("ledd.swsscommon.SubscriberStateTable")
+    @mock.patch("ledd.swsscommon.Select")
+    def test_run_bad_select_return(self, mock_select, mock_sst, mock_load_plat_util):
+        select_instance = mock_select.return_value
+        select_instance.select.return_value = (ledd.swsscommon.Select.ERROR, mock.MagicMock())
+
+        daemon_ledd = ledd.DaemonLedd()
+        ret = daemon_ledd.run()
+        assert ret == 2
+
+    @mock.patch("ledd.DaemonLedd.load_platform_util")
+    @mock.patch("ledd.swsscommon.CastSelectableToRedisSelectObj")
+    @mock.patch("ledd.swsscommon.SubscriberStateTable")
+    @mock.patch("ledd.swsscommon.Select")
+    def test_run_ignore_keys(self, mock_select, mock_sst, mock_cstrso, mock_load_plat_util):
+        select_instance = mock_select.return_value
+        select_instance.select.return_value = (ledd.swsscommon.Select.OBJECT, mock.MagicMock())
+
+        mock_cstrso.return_value.getDbConnector.return_value.getNamespace.return_value = ledd.multi_asic.DEFAULT_NAMESPACE
+
+        sst_instance = mock_sst.return_value
+
+        for key in ['PortConfigDone', 'PortInitDone']:
+            sst_instance.pop.return_value = ('PortConfigDone', 'SET', {'not': 'applicable'})
+
+            daemon_ledd = ledd.DaemonLedd()
+            ret = daemon_ledd.run()
+            assert ret == 3
+
+    @mock.patch("ledd.DaemonLedd.load_platform_util")
+    @mock.patch("ledd.swsscommon.CastSelectableToRedisSelectObj")
+    @mock.patch("ledd.swsscommon.SubscriberStateTable")
+    @mock.patch("ledd.swsscommon.Select")
+    def test_run_bad_fvp(self, mock_select, mock_sst, mock_cstrso, mock_load_plat_util):
+        select_instance = mock_select.return_value
+        select_instance.select.return_value = (ledd.swsscommon.Select.OBJECT, mock.MagicMock())
+
+        mock_cstrso.return_value.getDbConnector.return_value.getNamespace.return_value = ledd.multi_asic.DEFAULT_NAMESPACE
+
+        sst_instance = mock_sst.return_value
+
+        for fvp in [None, {}]:
+            sst_instance.pop.return_value = ('Ethernet0', 'SET', fvp)
+
+            daemon_ledd = ledd.DaemonLedd()
+            ret = daemon_ledd.run()
+            assert ret == 4
+
+    @mock.patch("ledd.DaemonLedd.load_platform_util")
+    @mock.patch("ledd.swsscommon.CastSelectableToRedisSelectObj")
+    @mock.patch("ledd.swsscommon.SubscriberStateTable")
+    @mock.patch("ledd.swsscommon.Select")
+    def test_run_good(self, mock_select, mock_sst, mock_cstrso, mock_led_control):
+        select_instance = mock_select.return_value
+        select_instance.select.return_value = (ledd.swsscommon.Select.OBJECT, mock.MagicMock())
+
+        mock_cstrso.return_value.getDbConnector.return_value.getNamespace.return_value = ledd.multi_asic.DEFAULT_NAMESPACE
+
+        sst_instance = mock_sst.return_value
+
+        led_control_instance = mock_led_control.return_value
+
+        for port in ['Ethernet0', 'Ethernet4']:
+            for link_state in ['up', 'down']:
+                sst_instance.pop.return_value = (port, 'SET', {'oper_status': link_state})
+
+                daemon_ledd = ledd.DaemonLedd()
+                ret = daemon_ledd.run()
+                assert ret == 0
+                assert led_control_instance.port_link_state_change.call_count == 1
+                led_control_instance.port_link_state_change.assert_called_with(port, link_state)
+                led_control_instance.port_link_state_change.reset_mock()
