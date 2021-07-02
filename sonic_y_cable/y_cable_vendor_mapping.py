01b8be3886d029645a7e5804de6aa90ab3e64959
xinyu <xinyu0123@gmail.com>
2021-07-02 18:19:52 +0800

    [Y_cable[Credo] remove PT1 bulge cable PN
    Signed-off-by: xinyu <xinyu0123@gmail.com>

--- sonic_y_cable/y_cable_vendor_mapping.py
+++ sonic_y_cable/y_cable_vendor_mapping.py
@@ -1,11 +1,10 @@
 mapping = {
     "Credo": {
-        "CAC125321P2PA0MS": "credo.y_cable_credo",
         "CACL05321P2PA1MS": "credo.y_cable_credo",
         "CACL1X321P2PA1MS": "credo.y_cable_credo",
         "CACL15321P2PA1MS": "credo.y_cable_credo",
         "CACL2X321P2PA1MS": "credo.y_cable_credo",
-        
+
         "CAC105321P2PA2MS": "credo.y_cable_credo",
         "CAC11X321P2PA2MS": "credo.y_cable_credo",
         "CAC115321P2PA2MS": "credo.y_cable_credo",
