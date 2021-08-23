c17c37065e9956593b2025ee7fe1362c6ffea64d
vdahiya12 <67608553+vdahiya12@users.noreply.github.com>
2021-08-23 14:06:50 -0700

    [Y-Cable][Broadcom] Broadcom implementation of YCable class which inherits from YCableBase required for Y-Cable API's in sonic-platform-daemons (#208)
    This PR adds support for YCable class required for platform-daemons to use the YCable API's for Broadcom.
    
    Description
    Basically a vendor specific implementation of abstract YCableBase class .
    detailed design discussion can be found https://github.com/Azure/SONiC/pull/757/files
    
    Motivation and Context
    Required for transitioning to vendor agnostic API's to be called by xcvrd, so that all types of cables can be called.
    
    How Has This Been Tested?
    Ran the changes on Arista7050cx3 switch, making changes inside the container.
    
    
    Signed-off-by: vaibhav-dahiya <vdahiya@microsoft.com>

--- sonic_y_cable/y_cable_vendor_mapping.py
+++ sonic_y_cable/y_cable_vendor_mapping.py
@@ -9,5 +9,19 @@ mapping = {
         "cac11x321p2pa2ms": "credo.y_cable_credo",
         "cac115321p2pa2ms": "credo.y_cable_credo",
         "cac12x321p2pa2ms": "credo.y_cable_credo"
+    },
+    "molex": {
+        "2164351001": "broadcom.y_cable_broadcom",
+        "2164351002": "broadcom.y_cable_broadcom",
+        "2164351003": "broadcom.y_cable_broadcom",
+        "2164351004": "broadcom.y_cable_broadcom",
+        "2164352001": "broadcom.y_cable_broadcom",
+        "2164352002": "broadcom.y_cable_broadcom",
+        "2164352003": "broadcom.y_cable_broadcom",
+        "2164352004": "broadcom.y_cable_broadcom",
+        "2164352501": "broadcom.y_cable_broadcom",
+        "2164352502": "broadcom.y_cable_broadcom",
+        "2164352503": "broadcom.y_cable_broadcom",
+        "2164352504": "broadcom.y_cable_broadcom"
     }
 }
