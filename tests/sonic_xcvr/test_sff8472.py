48a856fda95e5942b9f2f7285e12151d25d3fa6d
Andy Wong <andywong@arista.com>
2021-11-03 16:29:00 -0700

    Add tests

--- tests/sonic_xcvr/test_sff8472.py
+++ tests/sonic_xcvr/test_sff8472.py
@@ -1,9 +1,12 @@
 from mock import MagicMock
+import sys
 
 from sonic_platform_base.sonic_xcvr.api.public.sff8472 import Sff8472Api
 from sonic_platform_base.sonic_xcvr.codes.public.sff8472 import Sff8472Codes
+from sonic_platform_base.sonic_xcvr.fields.public.sff8472 import VoltageField
 from sonic_platform_base.sonic_xcvr.mem_maps.public.sff8472 import Sff8472MemMap
 from sonic_platform_base.sonic_xcvr.xcvr_eeprom import XcvrEeprom
+from sonic_platform_base.sonic_xcvr.fields import consts
 
 class TestSff8472(object):
     codes = Sff8472Codes
@@ -12,6 +15,7 @@ class TestSff8472(object):
     writer = MagicMock()
     eeprom = XcvrEeprom(reader, writer, mem_map)
     api = Sff8472Api(eeprom)
+    is_py2 = sys.version_info < (3,)
 
     def test_api(self):
         """
@@ -47,3 +51,67 @@ class TestSff8472(object):
         self.api.get_transceiver_thresholds_support()
         self.api.get_lpmode_support()
         self.api.get_power_override_support()
+
+    def test_temp(self):
+        temp_field = self.mem_map.get_field(consts.TEMPERATURE_FIELD)
+        data = bytearray([0x80, 0x00])
+        deps = {
+           consts.INT_CAL_FIELD: True,
+           consts.EXT_CAL_FIELD: False,
+           consts.T_SLOPE_FIELD: 1,
+           consts.T_OFFSET_FIELD: 0,
+        }
+        decoded = temp_field.decode(data, **deps)
+        assert decoded == -128
+
+    def test_voltage(self):
+        voltage_field = self.mem_map.get_field(consts.VOLTAGE_FIELD)
+        data = bytearray([0xFF, 0xFF])
+        deps = {
+           consts.INT_CAL_FIELD: True,
+           consts.EXT_CAL_FIELD: False,
+           consts.T_SLOPE_FIELD: 1,
+           consts.T_OFFSET_FIELD: 0,
+        }
+        decoded = voltage_field.decode(data, **deps)
+        expected = 6 if self.is_py2 else 6.5535
+        assert decoded == expected
+
+    def test_tx_bias(self):
+        tx_bias_field = self.mem_map.get_field(consts.TX_BIAS_FIELD)
+        data = bytearray([0xFF, 0xFF])
+        deps = {
+           consts.INT_CAL_FIELD: True,
+           consts.EXT_CAL_FIELD: False,
+           consts.T_SLOPE_FIELD: 1,
+           consts.T_OFFSET_FIELD: 0,
+        }
+        decoded = tx_bias_field.decode(data, **deps)
+        expected = 131 if self.is_py2 else 131.07
+        assert decoded == expected
+
+    def test_tx_power(self):
+        tx_power_field = self.mem_map.get_field(consts.TX_POWER_FIELD)
+        data = bytearray([0xFF, 0xFF])
+        deps = {
+           consts.INT_CAL_FIELD: True,
+           consts.EXT_CAL_FIELD: False,
+           consts.T_SLOPE_FIELD: 1,
+           consts.T_OFFSET_FIELD: 0,
+        }
+        decoded = tx_power_field.decode(data, **deps)
+        expected = 6 if self.is_py2 else 6.5535
+        assert decoded == expected
+
+    def test_rx_power(self):
+        rx_power_field = self.mem_map.get_field(consts.RX_POWER_FIELD)
+        data = bytearray([0xFF, 0xFF])
+        deps = {
+           consts.INT_CAL_FIELD: True,
+           consts.EXT_CAL_FIELD: False,
+           consts.T_SLOPE_FIELD: 1,
+           consts.T_OFFSET_FIELD: 0,
+        }
+        decoded = rx_power_field.decode(data, **deps)
+        expected = 6 if self.is_py2 else 6.5535
+        assert decoded == expected
