aa86083707c8f6eff24e02df1e5f2198259a8086
Junchao-Mellanox <57339448+Junchao-Mellanox@users.noreply.github.com>
2022-11-08 19:53:57 +0800

    Fix issue: rounding float value for txpower and rxpower (#320)
    - Description
    For cmis cable, tx power and rx power is not rounding as other eeprom fields
    
    - Motivation and Context
    Fix issue: rounding float value for txpower and rxpower
    
    - How Has This Been Tested?
    Manual test

--- sonic_platform_base/sonic_xcvr/api/public/cmis.py
+++ sonic_platform_base/sonic_xcvr/api/public/cmis.py
@@ -208,8 +208,8 @@ class CmisApi(XcvrApi):
         for i in range(1, self.NUM_CHANNELS + 1):
             bulk_status["tx%ddisable" % i] = tx_disable[i-1] if self.get_tx_disable_support() else 'N/A'
             bulk_status["tx%dbias" % i] = tx_bias['LaserBiasTx%dField' % i] if self.get_tx_bias_support() else 'N/A'
-            bulk_status["rx%dpower" % i] = self.mw_to_dbm(rx_power[i - 1]) if self.get_rx_power_support() else 'N/A'
-            bulk_status["tx%dpower" % i] = self.mw_to_dbm(tx_power[i - 1]) if self.get_tx_power_support() else 'N/A'
+            bulk_status["rx%dpower" % i] = float("{:.3f}".format(self.mw_to_dbm(rx_power[i - 1]))) if self.get_rx_power_support() else 'N/A'
+            bulk_status["tx%dpower" % i] = float("{:.3f}".format(self.mw_to_dbm(tx_power[i - 1]))) if self.get_tx_power_support() else 'N/A'
 
         laser_temp_dict = self.get_laser_temperature()
         self.vdm_dict = self.get_vdm()
