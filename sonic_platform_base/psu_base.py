94595a89d984b4a2d0886793f6240a127470993d
Stephen Sun <5379172+stephenxs@users.noreply.github.com>
2022-11-22 06:52:02 +0800

    Add warning/critical thresholds for PSU power (#304)
    Add warning/critical thresholds for PSU power
    
    Signed-off-by: Stephen Sun <stephens@nvidia.com>

--- sonic_platform_base/psu_base.py
+++ sonic_platform_base/psu_base.py
@@ -223,6 +223,26 @@ class PsuBase(device_base.DeviceBase):
         """
         raise NotImplementedError
 
+    def get_psu_power_warning_suppress_threshold(self):
+        """
+        Retrieve the warning suppress threshold of the power on this PSU
+        The value can be volatile, so the caller should call the API each time it is used.
+
+        Returns:
+            A float number, the warning suppress threshold of the PSU in watts.
+        """
+        raise NotImplementedError
+
+    def get_psu_power_critical_threshold(self):
+        """
+        Retrieve the critical threshold of the power on this PSU
+        The value can be volatile, so the caller should call the API each time it is used.
+
+        Returns:
+            A float number, the critical threshold of the PSU in watts.
+        """
+        raise NotImplementedError
+
     @classmethod
     def get_status_master_led(cls):
         """
