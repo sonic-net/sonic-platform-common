1fbc86496bcf23c3335341a95a08d64278ca794b
mihirpat1 <112018033+mihirpat1@users.noreply.github.com>
2023-01-25 09:49:05 -0800

    Change get_tx_bias return type to list (#342)
    * Change get_tx_bias return type to list
    
    Signed-off-by: Mihir Patel <patelmi@microsoft.com>

--- sonic_platform_base/sonic_xcvr/api/public/cmis.py
+++ sonic_platform_base/sonic_xcvr/api/public/cmis.py
@@ -207,7 +207,7 @@ class CmisApi(XcvrApi):
 
         for i in range(1, self.NUM_CHANNELS + 1):
             bulk_status["tx%ddisable" % i] = tx_disable[i-1] if self.get_tx_disable_support() else 'N/A'
-            bulk_status["tx%dbias" % i] = tx_bias['LaserBiasTx%dField' % i] if self.get_tx_bias_support() else 'N/A'
+            bulk_status["tx%dbias" % i] = tx_bias[i - 1]
             bulk_status["rx%dpower" % i] = float("{:.3f}".format(self.mw_to_dbm(rx_power[i - 1]))) if self.get_rx_power_support() else 'N/A'
             bulk_status["tx%dpower" % i] = float("{:.3f}".format(self.mw_to_dbm(tx_power[i - 1]))) if self.get_tx_power_support() else 'N/A'
 
@@ -495,7 +495,7 @@ class CmisApi(XcvrApi):
         tx_bias = self.xcvr_eeprom.read(consts.TX_BIAS_FIELD)
         for key, value in tx_bias.items():
             tx_bias[key] *= scale
-        return tx_bias
+        return [tx_bias['LaserBiasTx%dField' % i] for i in range(1, self.NUM_CHANNELS + 1)]
 
     def get_tx_power(self):
         '''
