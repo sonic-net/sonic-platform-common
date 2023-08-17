384177cacf08532fdfa3f339159e6674bbac9ae9
ChiouRung Haung <chiourung_huang@edge-core.com>
2023-08-18 01:38:22 +0800

    Convert the tx/rx power unit to the dBm unit (#377)
    * JIRA-SONIC-7072: Convert the tx/rx power unit to the dBm unit in get_transceiver_bulk_status().
    
    The tx/rx power unit in the eeprom is mW, and the tx/rx power unit for the API is dBm.
    It should be converted to the dBm in get_transceiver_bulk_status().
    
    Signed-off-by: chiourung_huang <chiourung_huang@edge-core.com>
    
    
    ---------
    
    Signed-off-by: chiourung_huang <chiourung_huang@edge-core.com>
    Co-authored-by: Prince George <45705344+prgeor@users.noreply.github.com>

--- tests/sonic_xcvr/test_sff8436.py
+++ tests/sonic_xcvr/test_sff8436.py
@@ -201,3 +201,50 @@ class TestSff8436(object):
         self.api.get_tx_disable.return_value = mock_response[3]
         result = self.api.get_transceiver_status()
         assert result == expected
+
+    @pytest.mark.parametrize("mock_response, expected",[
+        (
+            [
+                50,
+                3.3,
+                [70, 70, 70, 70],
+                [0.1, 0.1, 0.1, 0.1],
+                [0.1, 0.1, 0.1, 0.1],
+                True, True, True, True, True, True
+            ],
+            {
+                'temperature': 50,
+                'voltage': 3.3,
+                'tx1power': -10.0, 'tx2power': -10.0, 'tx3power': -10.0, 'tx4power': -10.0,
+                'rx1power': -10.0, 'rx2power': -10.0, 'rx3power': -10.0, 'rx4power': -10.0,
+                'tx1bias': 70, 'tx2bias': 70, 'tx3bias': 70, 'tx4bias': 70,
+            }
+        )
+    ])
+    def test_get_transceiver_bulk_status(self, mock_response, expected):
+        self.api.get_module_temperature = MagicMock()
+        self.api.get_module_temperature.return_value = mock_response[0]
+        self.api.get_voltage = MagicMock()
+        self.api.get_voltage.return_value = mock_response[1]
+        self.api.get_tx_bias = MagicMock()
+        self.api.get_tx_bias.return_value = mock_response[2]
+        self.api.get_rx_power = MagicMock()
+        self.api.get_rx_power.return_value = mock_response[3]
+        self.api.get_tx_power = MagicMock()
+        self.api.get_tx_power.return_value = mock_response[4]
+        self.api.get_rx_los_support = MagicMock()
+        self.api.get_rx_los_support.return_value = mock_response[5]
+        self.api.get_tx_fault_support = MagicMock()
+        self.api.get_tx_fault_support.return_value = mock_response[6]
+        self.api.get_tx_disable_support = MagicMock()
+        self.api.get_tx_disable_support.return_value = mock_response[7]
+        self.api.get_tx_bias_support = MagicMock()
+        self.api.get_tx_bias_support.return_value = mock_response[8]
+        self.api.get_tx_power_support = MagicMock()
+        self.api.get_tx_power_support.return_value = mock_response[9]
+        self.api.get_rx_power_support = MagicMock()
+        self.api.get_rx_power_support.return_value = mock_response[10]
+        result = self.api.get_transceiver_bulk_status()
+        assert result == expected
+
+
