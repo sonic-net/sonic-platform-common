4c39ae9cfa741c3e2442712265a6a18fd2dfb6cf
Anoop Kamath <115578705+AnoopKamath@users.noreply.github.com>
2023-10-04 13:28:46 -0700

    Address review comments
    1. Cleanup unnecessary imports 
    2. update the comments

--- sonic_platform_base/sonic_xcvr/api/credo/aec_800g.py
+++ sonic_platform_base/sonic_xcvr/api/credo/aec_800g.py
@@ -1,16 +1,11 @@
 """
     aec_800g.py
 
-    Implementation of Vendor specific in addition to the CMIS specification.
+    Implementation of Credo AEC cable specific in addition to the CMIS specification.
 """
 
 from ...fields import consts
-import logging
 from ..public.cmis import CmisApi
-import time
-
-logger = logging.getLogger(__name__)
-logger.addHandler(logging.NullHandler())
 
 class CmisAec800gApi(CmisApi):
     def set_firmware_download_target_end(self, target):
