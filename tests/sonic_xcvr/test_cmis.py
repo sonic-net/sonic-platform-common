6fa938fec7b4bb86945b25320772d98b662b6f29
Junchao-Mellanox <junchao@nvidia.com>
2023-10-10 11:41:03 +0300

    Increate code coverage

--- tests/sonic_xcvr/test_cmis.py
+++ tests/sonic_xcvr/test_cmis.py
@@ -2372,6 +2372,37 @@ class TestCmis(object):
         result = self.api.get_transceiver_info_firmware_versions()
         assert result == ["N/A", "N/A"]
 
+    def test_hex_dump(self):
+        data = bytearray([x for x in range(128)])
+        output = self.api.hexdump(' ' * 8, data, 0)
+        expected_output = """        00000000 00 01 02 03 04 05 06 07  08 09 0a 0b 0c 0d 0e 0f |................|
+        00000010 10 11 12 13 14 15 16 17  18 19 1a 1b 1c 1d 1e 1f |................|
+        00000020 20 21 22 23 24 25 26 27  28 29 2a 2b 2c 2d 2e 2f | !"#$%&'()*+,-./|
+        00000030 30 31 32 33 34 35 36 37  38 39 3a 3b 3c 3d 3e 3f |0123456789:;<=>?|
+        00000040 40 41 42 43 44 45 46 47  48 49 4a 4b 4c 4d 4e 4f |@ABCDEFGHIJKLMNO|
+        00000050 50 51 52 53 54 55 56 57  58 59 5a 5b 5c 5d 5e 5f |PQRSTUVWXYZ[\]^_|
+        00000060 60 61 62 63 64 65 66 67  68 69 6a 6b 6c 6d 6e 6f |`abcdefghijklmno|
+        00000070 70 71 72 73 74 75 76 77  78 79 7a 7b 7c 7d 7e 7f |pqrstuvwxyz{|}~.|"""
+        assert output == expected_output
+
+        data = bytearray([x for x in range(23)])
+        output = self.api.hexdump(' ' * 8, data, 0)
+        expected_output = """        00000000 00 01 02 03 04 05 06 07  08 09 0a 0b 0c 0d 0e 0f |................|
+        00000010 10 11 12 13 14 15 16                             |.......|"""
+        assert output == expected_output
+
+        data = bytearray([x for x in range(24)])
+        output = self.api.hexdump(' ' * 8, data, 0)
+        expected_output = """        00000000 00 01 02 03 04 05 06 07  08 09 0a 0b 0c 0d 0e 0f |................|
+        00000010 10 11 12 13 14 15 16 17                          |........|"""
+        assert output == expected_output
+
+        data = bytearray([x for x in range(25)])
+        output = self.api.hexdump(' ' * 8, data, 0)
+        expected_output = """        00000000 00 01 02 03 04 05 06 07  08 09 0a 0b 0c 0d 0e 0f |................|
+        00000010 10 11 12 13 14 15 16 17  18                      |.........|"""
+        assert output == expected_output
+
     def test_dump_eeprom(self):
         with pytest.raises(ValueError):
             self.api.dump_eeprom(-1)
