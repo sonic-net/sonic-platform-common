cb80f176d20bcb4079bd4314a09f7183bc6a8c53
Stephen Sun <5379172+stephenxs@users.noreply.github.com>
2023-11-22 14:39:40 +0800

    Fix issue: QSFP module with id 0x0d can be parsed using 8636 (#412)
    1. It should first read the revision compliance to decide which standard to use
       8636 should be used if it is greater than 3
    2. For 8636 "Specification compliance", the bit 7 should be ignored and be parsed in "Extended Specification Compliance"
    
    Signed-off-by: Stephen Sun <stephens@nvidia.com>

--- tests/sonic_xcvr/test_xcvr_api_factory.py
+++ tests/sonic_xcvr/test_xcvr_api_factory.py
@@ -8,6 +8,14 @@ from sonic_platform_base.sonic_xcvr.xcvr_eeprom import XcvrEeprom
 from sonic_platform_base.sonic_xcvr.codes.credo.aec_800g import CmisAec800gCodes
 from sonic_platform_base.sonic_xcvr.fields import consts
 from sonic_platform_base.sonic_xcvr.xcvr_api_factory import XcvrApiFactory
+from sonic_platform_base.sonic_xcvr.api.public.sff8636 import Sff8636Api
+from sonic_platform_base.sonic_xcvr.api.public.sff8436 import Sff8436Api
+
+def mock_reader_sff8636(start, length):
+    return bytes([0x0d]) if start == 0 else bytes ([0x06])
+
+def mock_reader_sff8436(start, length):
+    return bytes([0x0d]) if start == 0 else bytes ([0x00])
 
 class BytesMock(bytes):
     def decode(self, encoding='utf-8', errors='strict'):
@@ -45,3 +53,11 @@ class TestXcvrApiFactory(object):
         CmisAec800gApi = MagicMock()
         self.api.create_xcvr_api()
 
+    @pytest.mark.parametrize("reader, expected_api", [
+        (mock_reader_sff8636, Sff8636Api),
+        (mock_reader_sff8436, Sff8436Api),
+    ])
+    def test_create_xcvr_api_8436_8636(self, reader, expected_api):
+        self.api.reader = reader
+        api = self.api.create_xcvr_api()
+        assert isinstance(api, expected_api)
