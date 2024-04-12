9d8c3db23018dddfae1af5bc5df251b16b86d9a7
Mihir Patel <patelmi@microsoft.com>
2024-04-12 18:44:22 +0000

    Moved TARGET_MODE to Credo specific memmap

--- sonic_platform_base/sonic_xcvr/mem_maps/credo/aec_800g.py
+++ sonic_platform_base/sonic_xcvr/mem_maps/credo/aec_800g.py
@@ -5,7 +5,16 @@
 """
 
 from ..public.cmisTargetFWUpgrade import CmisTargetFWUpgradeMemMap
+from ...fields.xcvr_field import (
+    NumberRegField,
+    RegGroupField,
+)
+from ...fields import consts
 
 class CmisAec800gMemMap(CmisTargetFWUpgradeMemMap):
-    # Vendor specific implementation to be added here
-    pass
+    def __init__(self, codes):
+        super().__init__(codes)
+
+        self.VENDOR_CUSTOM = RegGroupField(consts.VENDOR_CUSTOM,
+            NumberRegField(consts.TARGET_MODE, self.getaddr(0x0, 64), ro=False)
+        )
--- sonic_platform_base/sonic_xcvr/mem_maps/public/cmisTargetFWUpgrade.py
+++ sonic_platform_base/sonic_xcvr/mem_maps/public/cmisTargetFWUpgrade.py
@@ -6,19 +6,7 @@
 """
 
 from .cmis import CmisMemMap
-from ...fields.xcvr_field import (
-    NumberRegField,
-    RegGroupField,
-)
-from ...fields import consts
 
 class CmisTargetFWUpgradeMemMap(CmisMemMap):
-    def __init__(self, codes):
-        super().__init__(codes)
-
-        self.VENDOR_CUSTOM = RegGroupField(consts.VENDOR_CUSTOM,
-            NumberRegField(consts.TARGET_MODE, self.getaddr(0x0, 64), ro=False)
-        )
-
-    def getaddr(self, page, offset, page_size=128):
-        return page * page_size + offset
+    # Vendor agnostic implementation to be added here
+    pass
