9b53bcb554abd1a32f0ff48a41717c141fb1a030
mihirpat1 <112018033+mihirpat1@users.noreply.github.com>
2024-04-14 00:26:48 -0700

    Separate common code for remote target FW upgrade supported optics (#453)
    * Separate common code for remote side FW upgrade supported optics
    
    Signed-off-by: Mihir Patel <patelmi@microsoft.com>
    
    * Added newline to EOF
    
    * Moved TARGET_MODE to Credo specific memmap
    
    ---------
    
    Signed-off-by: Mihir Patel <patelmi@microsoft.com>

--- sonic_platform_base/sonic_xcvr/mem_maps/credo/aec_800g.py
+++ sonic_platform_base/sonic_xcvr/mem_maps/credo/aec_800g.py
@@ -4,25 +4,17 @@
     Implementation of Credo AEC cable specific XcvrMemMap for CMIS Rev 5.0
 """
 
-from ..public.cmis import CmisMemMap
+from ..public.cmisTargetFWUpgrade import CmisTargetFWUpgradeMemMap
 from ...fields.xcvr_field import (
-    CodeRegField,
-    DateField,
-    HexRegField,
     NumberRegField,
-    RegBitField,
     RegGroupField,
-    StringRegField,
 )
 from ...fields import consts
 
-class CmisAec800gMemMap(CmisMemMap):
+class CmisAec800gMemMap(CmisTargetFWUpgradeMemMap):
     def __init__(self, codes):
-        super(CmisAec800gMemMap, self).__init__(codes)
+        super().__init__(codes)
 
         self.VENDOR_CUSTOM = RegGroupField(consts.VENDOR_CUSTOM,
             NumberRegField(consts.TARGET_MODE, self.getaddr(0x0, 64), ro=False)
         )
-
-    def getaddr(self, page, offset, page_size=128):
-        return page * page_size + offset
