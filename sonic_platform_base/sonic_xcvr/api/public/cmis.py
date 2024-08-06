4d919c8546aa5832bb977eb029fab9984ffaffd6
xinyu <xinyu0123@gmail.com>
2024-08-06 16:31:57 +0800

    [CMIS] Log get fw mgmt feature RPL check code result to provide clearer data when an error occurs
    Signed-off-by: xinyu <xinyu0123@gmail.com>

--- sonic_platform_base/sonic_xcvr/api/public/cmis.py
+++ sonic_platform_base/sonic_xcvr/api/public/cmis.py
@@ -1281,7 +1281,7 @@ class CmisApi(XcvrApi):
         else:
             txt += 'Status or reply payload check code error\n'
             logger.info(txt)
-            logger.info('Fail to get fw mgnt feature, cdb status: 0x{}\n'.format(status))
+            logger.info('Fail to get fw mgmt feature, cdb status: {:#x}, cdb_chkcode: {:#x}, rpl_chkcode: {:#x}\n'.format(status, self.cdb.cdb_chkcode(rpl), rpl_chkcode))
             return {'status': False, 'info': txt, 'feature': None}
         elapsedtime = time.time()-starttime
         logger.info('Get module FW upgrade features time: %.2f s\n' %elapsedtime)
