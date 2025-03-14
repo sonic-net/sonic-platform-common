56e6e8af7333533623e1c54641a38216c65468ba
Vaibhav Dahiya <vdahiya@microsoft.com>
2025-03-14 00:45:43 +0000

    add test
    Signed-off-by: Vaibhav Dahiya <vdahiya@microsoft.com>

--- tests/sonic_xcvr/test_cmis.py
+++ tests/sonic_xcvr/test_cmis.py
@@ -637,6 +637,7 @@ class TestCmis(object):
         (0, (0xff, True)),
         (0, (0, True)),
         (0, (0, False)),
+        (0, (1, False)),
         (None, (0, False))
     ])
     def test_rx_disable_channel(self, mock_response, input_param):
