4314b073207e2d45c5f4558b2f53b3af8c337965
Jianyue Wu <jianyuew@nvidia.com>
2025-05-09 09:06:58 +0000

    Support cache all non-empty collections
    Signed-off-by: Jianyue Wu <jianyuew@nvidia.com>

--- sonic_platform_base/sonic_xcvr/utils/cache.py
+++ sonic_platform_base/sonic_xcvr/utils/cache.py
@@ -1,14 +1,16 @@
+from collections import abc
+
 def read_only_cached_api_return(func):
-    """Cache until func() returns a non-None, non-empty dict/list value."""
+    """Cache until func() returns a non-None, non-empty collections cache_value."""
     cache_name = f'_{func.__name__}_cache'
     def wrapper(self):
         if not hasattr(self, cache_name):
-            val = func(self)
-            setattr(self, cache_name, val)
+            cache_value = func(self)
+            setattr(self, cache_name, cache_value)
         else:
-            val = getattr(self, cache_name)
-            if val is None or (isinstance(val, (dict, list)) and len(val) == 0):
-                val = func(self)
-                setattr(self, cache_name, val)
-        return val
+            cache_value = getattr(self, cache_name)
+            if cache_value is None or (isinstance(cache_value, abc.Iterable) and not cache_value):
+                cache_value = func(self)
+                setattr(self, cache_name, cache_value)
+        return cache_value
     return wrapper
