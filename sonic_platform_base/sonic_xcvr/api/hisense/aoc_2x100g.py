eeec1ec0262e9574538c6a3f724479cec51c549b
Pavan Kalyan Nakka <pnakka@microsoft.com>
2026-03-16 12:49:58 -0700

    Switched to CdbFwHandler for firmware info
    - Replaced CDB byte parsing with CdbFwHandler.get_firmware_info()
    - Updated tests to mock CdbFwHandler instead of raw CDB responses
    
    Signed-off-by: Pavan Kalyan Nakka <pnakka@microsoft.com>

--- sonic_platform_base/sonic_xcvr/api/hisense/aoc_2x100g.py
+++ sonic_platform_base/sonic_xcvr/api/hisense/aoc_2x100g.py
@@ -3,10 +3,10 @@
 
     Firmware management for modules that use single-bank firmware architecture.
 """
-import logging
 from ..public.cmis import CmisApi
-
-logger = logging.getLogger(__name__)
+from ...cdb.cdb_fw import CdbFwHandler
+from ...codes.public.cdb import CdbCodes
+from ...mem_maps.public.cdb import CdbMemMap
 
 class CmisAocSingleBankApi(CmisApi):
     def get_module_fw_info(self):
@@ -18,71 +18,64 @@ class CmisAocSingleBankApi(CmisApi):
         Validity Status: 1 = invalid, 0 = valid
         """
         txt = ''
-
         if self.cdb is None:
             return {'status': False, 'info': "CDB Not supported", 'result': None}
+        try:
+            fw_hdlr = CdbFwHandler(
+                self.xcvr_eeprom.reader,
+                self.xcvr_eeprom.writer,
+                CdbMemMap(CdbCodes)
+            )
+        except Exception:
+            return {'status': False, 'info': "CDB FW handler init failed", 'result': None}
+        
+        fw_info = fw_hdlr.get_firmware_info()
+        if not fw_info:
+            return {'status': False, 'info': "Failed to get firmware info", 'result': None}
+        
+        fw_status = fw_info.get('Cdb1FirmwareStatus', {})
+        ImageARunning = int(fw_status.get('CdbBankAOperStatus', False))
+        ImageACommitted = int(fw_status.get('CdbBankAAdminStatus', False))
+        ImageAValid = int(fw_status.get('CdbBankAValidStatus', True))
+        ImageBRunning = int(fw_status.get('CdbBankBOperStatus', False))
+        ImageBCommitted = int(fw_status.get('CdbBankBAdminStatus', False))
+        ImageBValid = int(fw_status.get('CdbBankBValidStatus', True))
 
-        # get fw info (CMD 0100h)
-        result = self.cdb.get_fw_info()
-        status = result['status']
-        rpllen, rpl_chkcode, rpl = result['rpl']
-
-        # Interface NACK or timeout
-        if (rpllen is None) or (rpl_chkcode is None):
-            return {'status': False, 'info': "Interface fail", 'result': 0} # Return result 0 for distinguishing CDB is maybe in busy or failure.
-
-        # password issue
-        if status == 0x46:
-            string = 'Get module FW info: Need to enter password\n'
-            logger.info(string)
-            # Reset password for module using CMIS 4.0
-            self.cdb.module_enter_password(0)
-            result = self.cdb.get_fw_info()
-            status = result['status']
-            rpllen, rpl_chkcode, rpl = result['rpl']
-
-        if status == 1 and self.cdb.cdb_chkcode(rpl) == rpl_chkcode:
-            # Register 9Fh:136
-            fwStatus = rpl[0]
-            ImageARunning = (fwStatus & 0x01) # bit 0 - image A is running
-            ImageACommitted = ((fwStatus >> 1) & 0x01) # bit 1 - image A is committed
-            ImageAValid = ((fwStatus >> 2) & 0x01) # bit 2 - image A is valid
-            ImageBRunning = ((fwStatus >> 4) & 0x01) # bit 4 - image B is running
-            ImageBCommitted = ((fwStatus >> 5) & 0x01)  # bit 5 - image B is committed
-            ImageBValid = ((fwStatus >> 6) & 0x01) # bit 6 - image B is valid
-
-            if ImageAValid == 0:
-                # Registers 9Fh:138,139; 140,141
-                ImageA = '%d.%d.%d' %(rpl[2], rpl[3], ((rpl[4]<< 8) | rpl[5]))
-            else:
-                ImageA = "N/A"
-            txt += 'Image A Version: %s\n' %ImageA
-
-            ImageB = "N/A"
-            txt += 'Image B Version: %s\n' %ImageB
-
-            if rpllen > 77:
-                factory_image = '%d.%d.%d' % (rpl[74], rpl[75], ((rpl[76] << 8) | rpl[77]))
-                txt += 'Factory Image Version: %s\n' %factory_image
-
-            ActiveFirmware = 'N/A'
-            InactiveFirmware = 'N/A'
-            if ImageARunning == 1:
-                RunningImage = 'A'
-                ActiveFirmware = ImageA
-                # In case of single bank module, inactive firmware version can be read from EEPROM
-                InactiveFirmware = self.get_module_inactive_firmware() + ".0"
-            else:
-                RunningImage = 'N/A'
-            if ImageACommitted == 1:
-                CommittedImage = 'A'
-            else:
-                CommittedImage = 'N/A'
-            txt += 'Running Image: %s\n' % (RunningImage)
-            txt += 'Committed Image: %s\n' % (CommittedImage)
-            txt += 'Active Firmware: {}\n'.format(ActiveFirmware)
-            txt += 'Inactive Firmware: {}\n'.format(InactiveFirmware)
+        if ImageAValid == 0:
+            ImageA = '{}.{}.{}'.format(
+                fw_info.get('CdbBankAMajorVersion', 0),
+                fw_info.get('CdbBankAMinorVersion', 0),
+                fw_info.get('CdbBankABuildVersion', 0)
+            )
         else:
-            txt += 'Reply payload check code error\n'
-            return {'status': False, 'info': txt, 'result': None}
+            ImageA = "N/A"
+        txt += 'Image A Version: {}\n'.format(ImageA)
+        ImageB = "N/A"
+        txt += 'Image B Version: {}\n'.format(ImageB)
+        
+        FactoryImage = '{}.{}.{}'.format(
+            fw_info.get('CdbFactoryMajorVersion', 0),
+            fw_info.get('CdbFactoryMinorVersion', 0),
+            fw_info.get('CdbFactoryBuildVersion', 0)
+        )
+        txt += 'Factory Image Version: {}\n'.format(FactoryImage)
+
+        ActiveFirmware = 'N/A'
+        InactiveFirmware = 'N/A'
+        if ImageARunning == 1:
+            RunningImage = 'A'
+            ActiveFirmware = ImageA
+            # In case of single bank module, inactive firmware version can be read from EEPROM
+            InactiveFirmware = self.get_module_inactive_firmware() + ".0"
+        else:
+            RunningImage = 'N/A'
+        if ImageACommitted == 1:
+            CommittedImage = 'A'
+        else:
+            CommittedImage = 'N/A'
+
+        txt += 'Running Image: {}\n'.format(RunningImage)
+        txt += 'Committed Image: {}\n'.format(CommittedImage)
+        txt += 'Active Firmware: {}\n'.format(ActiveFirmware)
+        txt += 'Inactive Firmware: {}\n'.format(InactiveFirmware)
         return {'status': True, 'info': txt, 'result': (ImageA, ImageARunning, ImageACommitted, ImageAValid, ImageB, ImageBRunning, ImageBCommitted, ImageBValid, ActiveFirmware, InactiveFirmware)}
--- tests/sonic_xcvr/test_aoc_2x100g.py
+++ tests/sonic_xcvr/test_aoc_2x100g.py
@@ -1,11 +1,22 @@
-from unittest.mock import patch
-from mock import MagicMock
+from unittest.mock import patch, MagicMock
 import pytest
 
 from sonic_platform_base.sonic_xcvr.mem_maps.public.cmis import CmisMemMap
 from sonic_platform_base.sonic_xcvr.xcvr_eeprom import XcvrEeprom
 from sonic_platform_base.sonic_xcvr.codes.public.cmis import CmisCodes
 from sonic_platform_base.sonic_xcvr.api.hisense.aoc_2x100g import CmisAocSingleBankApi
+
+def _compose_fw_info(a_oper, a_admin, a_valid, b_oper, b_admin, b_valid, a_maj, a_min, a_bld, b_maj, b_min, b_bld, f_maj, f_min, f_bld, img_info=7):
+    return {
+        'Cdb1FirmwareStatus': {
+            'CdbBankAOperStatus': a_oper, 'CdbBankAAdminStatus': a_admin, 'CdbBankAValidStatus': a_valid,
+            'CdbBankBOperStatus': b_oper, 'CdbBankBAdminStatus': b_admin, 'CdbBankBValidStatus': b_valid,
+        },
+        'CdbImageInfo': img_info,
+        'CdbBankAMajorVersion': a_maj, 'CdbBankAMinorVersion': a_min, 'CdbBankABuildVersion': a_bld,
+        'CdbBankBMajorVersion': b_maj, 'CdbBankBMinorVersion': b_min, 'CdbBankBBuildVersion': b_bld,
+        'CdbFactoryMajorVersion': f_maj, 'CdbFactoryMinorVersion': f_min, 'CdbFactoryBuildVersion': f_bld,
+    }
 
 class TestCmisAocSingleBankApi(object):
     codes = CmisCodes
@@ -16,29 +27,53 @@ class TestCmisAocSingleBankApi(object):
     api = CmisAocSingleBankApi(eeprom)
 
     @pytest.mark.parametrize("mock_response, expected", [
-        ({'status': 1, 'rpl': (128, 1, [0x43, 0, 2, 5, 0, 3] + [0] * 122)}, {'status': True, 'info': "", 'result': 
-        ('2.5.3', 1, 1, 0, 'N/A', 0, 0, 1, '2.5.3', '1.1.0')}),
-        ({'status': 1, 'rpl': (128, 1, [0x40, 0, 1, 0, 0, 1] + [0] * 122)}, {'status': True, 'info': "", 'result': 
-        ('1.0.1', 0, 0, 0, 'N/A', 0, 0, 1, 'N/A', 'N/A')}),
-        ({'status': 1, 'rpl': (128, 1, [0x44] + [0] * 127)}, {'status': True, 'info': "", 'result': ('N/A', 0, 0, 1, 'N/A', 0, 0, 1, 'N/A', 'N/A')}),
-        ({'status': 1, 'rpl': (None, 1, [0] * 128)}, {'status': False, 'info': "", 'result': 0}),
-        ({'status': 1, 'rpl': (128, None, [0] * 128)}, {'status': False, 'info': "", 'result': 0}),
-        ({'status': 1, 'rpl': (128, 0, [0] * 128)}, {'status': False, 'info': "", 'result': None}),
-        ({'status': 0x46, 'rpl': (128, 0, [0] * 128)}, {'status': False, 'info': "", 'result': None}),
+        (_compose_fw_info(True,  True,  False, False, False, True,  2, 5, 3, 0, 0, 0, 1, 6, 0),
+         ('1.1', {'status': True, 'result': ('2.5.3', 1, 1, 0, 'N/A', 0, 0, 1, '2.5.3', '1.1.0')})),
+        (_compose_fw_info(False, False, False, False, False, True,  1, 0, 1, 0, 0, 0, 1, 0, 0),
+         ('0.0', {'status': True, 'result': ('1.0.1', 0, 0, 0, 'N/A', 0, 0, 1, 'N/A', 'N/A')})),
+        (_compose_fw_info(False, False, True,  False, False, True,  0, 0, 0, 0, 0, 0, 0, 0, 0),
+         ('0.0', {'status': True, 'result': ('N/A', 0, 0, 1, 'N/A', 0, 0, 1, 'N/A', 'N/A')})),
+        (_compose_fw_info(False, False, False, True,  True,  False, 1, 4, 0, 1, 3, 0, 1, 6, 0),
+         ('1.3', {'status': True, 'result': ('1.4.0', 0, 0, 0, 'N/A', 1, 1, 0, 'N/A', 'N/A')})),
     ])
-    def test_get_module_fw_info(self, mock_response, expected):
+
+    @patch('sonic_platform_base.sonic_xcvr.api.hisense.aoc_2x100g.CdbFwHandler')
+    def test_get_module_fw_info(self, mock_cdb_fw_cls, mock_response, expected):
+        mock_fw_hdlr = MagicMock()
+        mock_fw_hdlr.get_firmware_info.return_value = mock_response
+        mock_cdb_fw_cls.return_value = mock_fw_hdlr
         self.api.cdb = MagicMock()
-        self.api.cdb.cdb_chkcode = MagicMock()
-        self.api.cdb.cdb_chkcode.return_value = 1
-        self.api.get_module_inactive_firmware = MagicMock()
-        self.api.get_module_inactive_firmware.return_value = "1.1"
-        self.api.cdb.get_fw_info = MagicMock()
-        self.api.cdb.get_fw_info.return_value = mock_response
+        self.api.get_module_inactive_firmware = MagicMock(return_value=expected[0])
         result = self.api.get_module_fw_info()
-        assert result['status'] == expected['status']
-        assert result['result'] == expected['result']
+        assert result['status'] == expected[1]['status']
+        assert result['result'] == expected[1]['result']
 
     def test_get_module_fw_info_cdb_none(self):
         self.api.cdb = None
         result = self.api.get_module_fw_info()
         assert result == {'status': False, 'info': "CDB Not supported", 'result': None}
+
+    @patch('sonic_platform_base.sonic_xcvr.api.hisense.aoc_2x100g.CdbFwHandler')
+    def test_get_module_fw_info_handler_init_fail(self, mock_cdb_fw_cls):
+        mock_cdb_fw_cls.side_effect = Exception("Init failed")
+        self.api.cdb = MagicMock()
+        result = self.api.get_module_fw_info()
+        assert result == {'status': False, 'info': "CDB FW handler init failed", 'result': None}
+
+    @patch('sonic_platform_base.sonic_xcvr.api.hisense.aoc_2x100g.CdbFwHandler')
+    def test_get_module_fw_info_fw_info_none(self, mock_cdb_fw_cls):
+        mock_fw_hdlr = MagicMock()
+        mock_fw_hdlr.get_firmware_info.return_value = None
+        mock_cdb_fw_cls.return_value = mock_fw_hdlr
+        self.api.cdb = MagicMock()
+        result = self.api.get_module_fw_info()
+        assert result == {'status': False, 'info': "Failed to get firmware info", 'result': None}
+
+    @patch('sonic_platform_base.sonic_xcvr.api.hisense.aoc_2x100g.CdbFwHandler')
+    def test_get_module_fw_info_fw_info_empty(self, mock_cdb_fw_cls):
+        mock_fw_hdlr = MagicMock()
+        mock_fw_hdlr.get_firmware_info.return_value = {}
+        mock_cdb_fw_cls.return_value = mock_fw_hdlr
+        self.api.cdb = MagicMock()
+        result = self.api.get_module_fw_info()
+        assert result == {'status': False, 'info': "Failed to get firmware info", 'result': None}
